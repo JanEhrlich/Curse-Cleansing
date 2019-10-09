25322bfdc19b2a269577b2d6560629c4fa02e64f
Jan Ehrlich <jan.ehrlich.develop@gmail.com>
2019-10-09 21:54:46 +0200

    Added Notifiacation Box with Level Goals for all levels | Fix for Attack delay applied

---
+++ Assets/Scripts/NotificationBox.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NotificationBox : MonoBehaviour
+{
+    public TextMeshProUGUI textMeshPro;
+    public float displayDuration = 5;
+    public bool displayOnLevelStart = false;
+    public List<string> notificationTexts;
+
+    private Animator anim;
+    private float waitLevelStart = 0.5F;
+
+    private float timer;
+    private float onLevelStartTimer;
+
+    private bool isShowing;
+    private bool shownOnLevelStart = false;
+
+    private int popup = Animator.StringToHash("Popup");
+    private int popdown = Animator.StringToHash("Popdown");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isShowing)
+        {
+            if (timer < displayDuration)
+            {
+                timer += Time.deltaTime;
+            }
+            else
+            {
+                HideNotification();
+            }
+        }
+
+        if (displayOnLevelStart && !shownOnLevelStart)
+        {
+            if (onLevelStartTimer < waitLevelStart)
+            {
+                onLevelStartTimer += Time.deltaTime;
+            }
+            else
+            {
+                ShowNotification(0);
+                shownOnLevelStart = true;
+            }
+        }
+    }
+
+    private void HideNotification()
+    {
+        anim.Play(popdown);
+        isShowing = false;
+    }
+
+    public void ShowNotification(int textNumber)
+    {
+        if (textNumber >= 0 && textNumber < notificationTexts.Count)
+        {
+            textMeshPro.text = notificationTexts[textNumber];
+        }
+        else
+        {
+            Debug.Log("Notification Box Text Number out of Range");
+        }
+
+        anim.Play(popup);
+        timer = 0;
+        isShowing = true;
+    }
+}
